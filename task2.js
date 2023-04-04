6146817786b1dab7c8e1d77aac922cbb4018c0b0
Artur-Ned <advokat1707@gmail.com>
2023-04-04 16:40:04 +0200

    task 2 done

---
+++ task2.js
@@ -0,0 +1,17 @@
+
+
+let divTextAll = document.querySelectorAll(".div-text");
+let btn = document.querySelector(".div-text-btn");
+
+// console.log(divTextAll);
+
+for (let div of divTextAll) {
+    div.addEventListener("click", foo);
+    function foo(e) {
+        if (e.target.nodeName === "BUTTON") {
+            div.firstChild.textContent = " ";
+        }
+    }
+}
+
+
