fbfda98de2bb04696f495428ddb8a1b24cb871c5
Artur-Ned <advokat1707@gmail.com>
2023-04-04 15:03:35 +0200

    1 task rinish

--- 001_HomeWork.js
+++ 001_HomeWork.js
@@ -1,20 +1,19 @@
 let yacor = document.querySelector("#yacor");
 let divText = document.querySelector(".div-text");
 let btn = document.querySelector(".btn");
-console.log(btn);
+// console.log(btn);
 
-    
+
 yacor.addEventListener("click", togle);
-
+yacor.onclick = function (e) { e.preventDefault() }
 function togle(e) {
-    e.preventDefault();
-    divText.classList.toggle("hidden");
-    
+       divText.classList.toggle("hidden");    
 }
+// yacor.onclick.preventDefault();
 
 btn.addEventListener("click", dellete);
-
 function dellete(e) {
-    yacor.removeEventListener("click", togle);  
-    
+    yacor.removeEventListener("click", togle);    
 }
+
+
